9bf850a5038374cabf358173fa42e62eabd83d53
jpUoN <j.pearson@nottingham.ac.uk>
2014-10-22 15:41:37 +0100

    plot
    plot

--- code/plotscripts/plotxy.m
+++ code/plotscripts/plotxy.m
@@ -4,6 +4,9 @@ function plotxy(varargin)
     rootdir='../';
     name = char(varargin(1));
     which = char(varargin(2));
+    whichID = char(varargin(3));
+    
+    
     datafx=strcat(rootdir,name,'/file','_x_',which,'.dat');
     datafy=strcat(rootdir,name,'/file','_y_',which,'.dat'); 
     datx = load(datafx);
@@ -11,12 +14,21 @@ function plotxy(varargin)
     x = datx(:,1);
     y = daty(:,1);
     
-    fx = datx(:,5);
-    fy = daty(:,5);
+    if whichID == 'cham'
+        ID = 2;
+    end;
+    
+    if whichID == 'force'
+        ID = 5;
+    end;
+    
+    ylab = whichID;
+    fx = datx(:,ID);
+    fy = daty(:,ID);
     
     plot(x,fx,y,fy);
     xlim([0 30]);
     xlabel('x');
-    ylabel('force');
+    ylabel(ylab);
     legend('F : x-dirn','F : y-dirn');
     legend boxoff;
