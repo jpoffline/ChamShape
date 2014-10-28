06f2a525ed9cbe6ba55363fc215ff98a34f8a2e4
jpUoN <j.pearson@nottingham.ac.uk>
2014-10-28 12:35:59 +0000

    plots
    plot

--- code/plotscripts/plotfrats.m
+++ code/plotscripts/plotfrats.m
@@ -1,12 +1,18 @@
 function plotfrats(varargin)
-
+    close all;
+    
     name = strcat(char(varargin(1)),'/');
     rootDIR='../';
     outDIR='output/';
+    plotDIR='plots/';
     forceinfofileproto='forceinfo.dat';
     
-    datafile=strcat(rootDIR,outDIR,name,forceinfofileproto);
+    plot_sizeunits='inches';
+    plot_width=4.5;
+    plot_height=3.5;
     
+    datafile=strcat(rootDIR,outDIR,name,forceinfofileproto);
+    outfigname=strcat(rootDIR,plotDIR,name,'_frats.pdf');
     dat=load(datafile);
     
     fratpos=5;
@@ -20,13 +26,20 @@ function plotfrats(varargin)
     M=dat(:,Mpos);
     
     ndat=size(dat);
-    nd=size(1);
+    nd=ndat(1);
     
     for n=1:nd
-        el=a(n)/b(n);
+        el=b(n)/a(n);
     end;
     
-    plot(el,frat);
-    xlabel('a/b');
-    ylabel('max(F_{cham} / F_{grav})');
+    plot(el,frat,'k-o','MarkerFaceColor','k');
+    
+    xlabel('$b/a$', 'interpreter','latex');
+    ylabel('$\mbox{max}(F_{\rm cham}{\,} / F_{\rm grav})$', 'interpreter','latex');
+    
+    set(gcf, 'PaperUnits',plot_sizeunits);
+    set(gcf, 'PaperSize',[plot_width plot_height]);
+    set(gcf, 'PaperPosition',[ 0 0 plot_width plot_height]);
+    set(gcf, 'renderer', 'painters');
+    print('-dpdf',outfigname);
     
