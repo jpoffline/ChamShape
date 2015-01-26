dc65881ad7313c83aee53c2122f0c1a8d177c90c
jpUoN <j.pearson@nottingham.ac.uk>
2015-01-26 14:00:24 +0000

    py

--- 1dcode/2d/plots_2d.py
+++ 1dcode/2d/plots_2d.py
@@ -4,25 +4,37 @@ import plotaux.plot_aux as aux
 
 
 buff = 1000
-#object_sizes = (3.0,1.0,0.3,0.1,0.03)
+
 object_radii = (1.5,0.5,0.15,0.05)
 
 NP = len(object_radii)
 
 for i in xrange(0,NP):  
      
-    fn = str(buff) + str(i)
-    filename = 'out/' + fn + '_force.dat'
+    fID = str(buff) + str(i)
     
-    (x,y,force) = aux.GetData(filename)    
-    (y,f) = aux.Get_x((x,y,force),0)
+    filename = 'out/' + fID + '_phi.dat'
+    
+    (x,y,F) = aux.GetData(filename)    
+    (y,phi) = aux.Get_x((x,y,F),0)
+    for j in xrange(0,len(y)):
+        y[j] = str(float(y[j]) - object_radii[i])
+    
+    
+    filename = 'out/' + fID + '_force.dat'
+    
+    (x,y,F) = aux.GetData(filename)    
+    (y,force) = aux.Get_x((x,y,F),0)
     for j in xrange(0,len(y)):
         y[j] = str(float(y[j]) - object_radii[i])
         
-    plt.plot( y, f ,label=str(object_radii[i]))
-    
+    plt.subplot(121)    
+    plt.plot( y, phi ,label = str(object_radii[i]) )
+    plt.subplot(122)    
+    plt.plot( y, force ,label = str(object_radii[i]) )
+    plt.xlim([0, 1.5])     
 
-#plt.xlim([0, 1.5])    
+   
 leg = plt.legend()   
 leg.draw_frame(False)
 plt.show()
