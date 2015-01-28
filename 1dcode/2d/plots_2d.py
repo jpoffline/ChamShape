6f5ba32b11ce6426d717fe060bc69062f59139b3
jpUoN <j.pearson@nottingham.ac.uk>
2015-01-28 10:10:50 +0000

    bug

--- 1dcode/2d/plots_2d.py
+++ 1dcode/2d/plots_2d.py
@@ -56,12 +56,12 @@ for i in xrange(0,NP):
     
         (x,y,F) = aux.GetData(filename)    
         (Y,force_y) = aux.Get_x((x,y,F),0)
-        for j in xrange(0,len(y)):
+        #for j in xrange(0,len(y)):
        #     y[j] = str(float(y[j]) - object_radii[i])
         
         (x,y,F) = aux.GetData(filename)    
         (X,force_x) = aux.Get_y((x,y,F),0)
-        for j in xrange(0,len(x)):
+        #for j in xrange(0,len(x)):
         #    x[j] = str(float(x[j]) - object_radii[i])
         
         plt.subplot(221)    
